ec2f7e83f46f5bb1d4b50052229e0cdca44aecc1
Andrea Scaudo <scaudo.andrea@gmail.com>
2022-03-03 17:44:21 +0100

    Unit Measure

--- acquisition/sed.py
+++ acquisition/sed.py
@@ -1,11 +1,14 @@
 class Sed:
     def __init__(
+
         self,
         spectrum_file: str,
-        temperature: float,
-        magnitude: float,
-        magnitude_system: str
+        temperature: float,             # K
+        magnitude: float,               # [-]
+        magnitude_system: str           # ["Vega" or "AB"]
+
     ) -> None:
+
         self.spectrum_file = spectrum_file
         self.temperature = temperature
         self.magnitude = magnitude
--- acquisition/sky.py
+++ acquisition/sky.py
@@ -1,11 +1,14 @@
 class Sky:
     def __init__(
+
         self,
-        airmass: str,
-        moon_fli: float,
-        pwv: float,
-        seeing: str
+        airmass: str,           # [-]
+        moon_fli: float,        # [days]
+        pwv: float,             # [mm]
+        seeing: str             # [arcsec]
+
     ) -> None:
+
         self.airmass = airmass
         self.moon_fli = moon_fli
         self.pwv = pwv
